0488a8c142b2929f1cd7b387c1b53767c34cc96b
jud512 <nagyjudit512@gmail.com>
2023-02-01 16:22:20 +0100

    create forms

--- src/components/createNewVocabulary/CreateNewVocabulary.jsx
+++ src/components/createNewVocabulary/CreateNewVocabulary.jsx
@@ -9,14 +9,18 @@ const CreateNewVocabulary = () => {
     const {topics} = useGlobalContext();
     const [book, setBook] = useState('BOOK1');
     const [selectedTopic, setSelectedTopic] = useState('');
+    const [selectedTopicId, setSelectedTopicId] = useState('');
+
     const handleChangeTopic = (e) => {
         setSelectedTopic(e.target.value);
         const foundItem = topics.find(item => item.title === selectedTopic);
         setBook(foundItem.book);
+        setSelectedTopicId(foundItem.id);
     }
     useEffect(() => {
         setBook(topics[0]?.book);
         setSelectedTopic(topics[0]?.title);
+        setSelectedTopicId(topics[0]?.id);
     }, [topics])
     //TODO!!!!!!!!!!!
     const createNewWord = async (e) => {
@@ -26,21 +30,30 @@ const CreateNewVocabulary = () => {
             const result = API.graphql(
                 graphqlOperation(createWord, {
                     input:{
-                        name: target.wordName,
-                        speech: target.speechName,
-                        description: target.descName,
-                        pronunciation: target.pronName,
-                        sound: target.soundName,
-                        example: target.exampleName,
-                        wordId: ''
+                        // name: target.wordName,
+                        // speech: target.speechName,
+                        // description: target.descName,
+                        // pronunciation: target.pronName,
+                        // sound: target.soundName,
+                        // example: target.exampleName,
+                        // topicId: selectedTopic,
+                        name: 'amphibian',
+                        speech: 'noun',
+                        description: "an animal of a class that typically lives in water when young and develops lungs as an adult, including frogs, toads, and salamanders",
+                        pronunciation: 'æm.ˈfɪ.biən',
+                        // sound: target.soundName,
+                        // example: target.exampleName,
+                        topicId: '583dc218-97ed-40cb-85c8-d1631c09a1e6',
                     }
                 })
             )
+            // console.log(target)
+            console.log(result)
         } catch (error) {
-            
+            console.log('WORD NOT SAVED', error)
         }
     }
-    
+    console.log('SELECTED',selectedTopicId)
     return (
         <div className='createNewVocab'>
             <h1 className='createVocTitle'>Add New Word To The Vocabulary</h1>
@@ -55,7 +68,9 @@ const CreateNewVocabulary = () => {
                                     ))
                                 }
                             </select>
+                            
                         </div>
+                        <p>TopicID: {selectedTopicId}</p>
                         <div className="formItem">
                             <span>Book</span>
                             <span>{book}</span>
@@ -86,7 +101,7 @@ const CreateNewVocabulary = () => {
                                 <input type="text" id='sound' name='souondName' />
                             </div>
                             
-                    <button className='btnSave' onClick={createNewWord}>Save it into the database</button>
+                    <button type='submit' className='btnSave' onClick={createNewWord}>Save it into the database</button>
                 </form>
             </div>
             
