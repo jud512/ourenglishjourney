9717bc58e0c8b567db84acb85ea6a4a0cbb18cfa
jud512 <nagyjudit512@gmail.com>
2023-03-13 21:13:27 +0100

    Task Edito styling

--- src/pages/taskEditor/TaskEditor.jsx
+++ src/pages/taskEditor/TaskEditor.jsx
@@ -1,23 +1,24 @@
 import React from 'react'
 import './TaskEditor.css'
 import { Link } from 'react-router-dom'
+import Header from '../../components/header/Header'
+import CircleOption from '../../components/circleOption/CircleOption'
 
 
 const TaskEditor = () => {
   return (
     <div className='taskEditor'>
-        <h1>Task Editor</h1> 
+        <Header title="Task Editor" img1="https://images.pexels.com/photos/235732/pexels-photo-235732.jpeg?auto=compress&cs=tinysrgb&w=1600" img2="https://images.pexels.com/photos/1586950/pexels-photo-1586950.jpeg?auto=compress&cs=tinysrgb&w=1600"/>
+        
         <p>Which task would you like to edit?</p>   
         <div className="taskEditor-options">
             <Link to="editrewritesentence">
-                <div className="taskEditor-rewrite" >
-                    <span>Rewrite Sentence App Database</span>
-                </div>
+                <CircleOption title="Rewrite Sentence App Database" background="orange"/>
+                
             </Link>
             <Link to="/">
-                <div className="taskEditor-another">
-                    <span>Another App database </span>
-                </div>
+                <CircleOption title="Another App database" background="darkblue"/>
+                
             </Link>
                 
 
